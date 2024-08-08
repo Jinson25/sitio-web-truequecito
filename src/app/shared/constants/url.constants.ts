4e2759f3f7747d99856e8cb3273869fe45376de1
Jinson25 <am987406@gmail.com>
2024-08-07 19:39:36 -0500

    Actualizar URL de intercambio en servicios de productos y de intercambio

--- src/app/shared/constants/url.constants.ts
+++ src/app/shared/constants/url.constants.ts
@@ -1,23 +1,5 @@
-/*const BASE_URL = 'http://localhost:5000/api';
 
-//AUTH
-export const URL_LOGIN = `${BASE_URL}/auth/login`;
-export const URL_REGISTER = `${BASE_URL}/auth/register`;
-export const URL_CALLBACK = `http://localhost:5000/auth/google/callback`;
-export const URL_GOOGLE = `${BASE_URL}/auth/google`;
-export const URL_DISCORD = `${BASE_URL}/auth/discord`;
-
-// PRODUCTS
-export const URL_PRODUCTS = `${BASE_URL}/products`;
-
-//search
-export const URL_SEARCH = `${BASE_URL}/products`;
-
-
-//PROFILE
-export const URL_PROFILE = `${BASE_URL}/auth/user`*/
-
-const BASE_URL = 'http://localhost:5000/api';
+const BASE_URL = 'https://api-rest-truequecito.onrender.com/api';
 
 // AUTH
 export const URL_LOGIN = `${BASE_URL}/auth/login`;
